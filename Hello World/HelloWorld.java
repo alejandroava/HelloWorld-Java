46ca9a4cfcdca8f2924f08656f6c4af74db2c96e
alejandroava <163916088+alejandroava@users.noreply.github.com>
2024-07-03 10:53:39 -0500

    Add files via upload

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World my name is Alejandro"); // Esto es un comentario
+    
+}
+}
+
+/*
+este es un comentario 
+en multiples lineas
+*/
